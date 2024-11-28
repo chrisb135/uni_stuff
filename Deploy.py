891484b70b5b7272905e0bd4baa5be4e96925aa1
Christian Laurence Almeida Barry <christianb06014@gmail.com>
2024-11-28 03:29:57 -0300

    Add files via upload

---
+++ Deploy.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+df = pd.read_csv("C:/Users/T-Gamer/Downloads/Student_Performance.csv")
+model_lr = joblib.load("C:/Users/T-Gamer/.vscode/.vscode/lr_model.pkl")
+
+st.title('Predição de Performance Estudantil')
+selecao = st.selectbox('Escolha a forma de previsão:', ['Previsão em massa (CSV)', 'Previsão individual'])
+if selecao == 'Previsão em massa (CSV)':
+  st.header('Previsão em massa')
+  uploaded_file = st.file_uploader('Escolha um arquivo CSV', type=['csv'])
+  if uploaded_file is not None:
+    alunos_csv = pd.read_csv(uploaded_file)
+    alunos_csv = alunos_csv.drop('Performance Index', axis=1)
+    alunos_csv = alunos_csv.drop('Extracurricular Activities', axis=1)
+    alunos_csv = alunos_csv.values
+  if st.button('Prever', 1):
+    predicao = model_lr.predict(alunos_csv)
+    st.write(predicao)
+elif selecao == 'Previsão individual':
+  st.header('Previsão individual')
+  horas_estudadas = st.slider("Horas estudadas", int(df['Hours Studied'].min()), int(df['Hours Studied'].max()))
+  notas_passadas = st.slider("Média de notas passadas", int(df['Previous Scores'].min()), int(df['Previous Scores'].max()))
+  horas_dormidas = st.slider("Média de horas dormidas", int(df['Sleep Hours'].min()), int(df['Sleep Hours'].max()))
+  papers_practiced = st.slider("Listas de exercícios resolvidas", int(df['Sample Question Papers Practiced'].min()), int(df['Sample Question Papers Practiced'].max()))
+  if st.button('Prever', 2):
+    data = np.array([[horas_estudadas, notas_passadas, horas_dormidas, papers_practiced]])
+    predicao = model_lr.predict(data)
+    st.header('Resultado')
+    st.write(predicao)
